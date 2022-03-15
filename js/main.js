73f3b5067e9b72604faa9dfa32640738d829c7a0
Krzysztof Markiewicz <krismarkiew@gmail.com>
2022-03-15 20:00:43 +0100

    change few element/ add offers

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    //dodaje cień do nawigacji
+    //add shadow to the navigation
     const navbar = document.querySelector('.navbar')
 
     function addShadowNav() {
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     window.addEventListener('scroll', addShadowNav)
 
-    //skrypt naprawiający nawigację, zamyka się po kliknięciu gdziekolwiek na mobilce
+    //fixes mobile navigation (when you click out of the navigation then navigation will be closed)
     $(document).click(function (event) {
         var clickover = $(event.target);
         var _opened = $(".navbar-collapse").hasClass("show");
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    //karuzela ze slick.js
+    //carousel from slick.js
 
     $('.carousel-slick').slick({
         autoplay: true,
@@ -52,7 +52,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
-
+//set Height of all offer-box to the height of the highest offer-box
 const offerBoxes = document.body.querySelectorAll('.offer-box')
 const setHeightofferBox = () => {
     const box = []
@@ -65,3 +65,31 @@ const setHeightofferBox = () => {
 }
 addEventListener('resize', setHeightofferBox)
 addEventListener('load', setHeightofferBox)
+
+
+
+//opens and closes offer(announcement_wrap)
+const offer = document.body.querySelectorAll('.announcement_wrap')
+const offerClose = document.body.querySelectorAll('.close_announcement')
+const offerBoxButton = document.body.querySelectorAll('.btn-offer')
+
+
+//open announcement_wrap
+offerBoxButton.forEach(el => {
+    el.addEventListener('click', () => {
+        const lastClassOfferBoxButton = el.className.split(' ') //make array from classes of button
+        const x = lastClassOfferBoxButton[lastClassOfferBoxButton.length - 1]; //get the last class of button
+        offer.forEach(el => {// change display of right announcement_wrap to flex 
+            if (el.classList.contains(`${x}`)) {
+                el.style.display = 'flex'
+            }
+
+        })
+    })
+})
+//close announcement_wrap
+offerClose.forEach(el => {
+    el.addEventListener('click', () => {
+        el.parentElement.style.display = 'none'
+    })
+})
